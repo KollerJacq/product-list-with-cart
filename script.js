8edb5fd4f833178ed2a87d18ae1daa7d00a68ec2
KollerJacq <147981831+KollerJacq@users.noreply.github.com>
2024-09-28 17:37:46 +0200

    added plus and minus symbols

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const buttons = document.querySelectorAll(".btn");
+const minusSymbol = document.querySelectorAll(".minus");
+const plusSymbol = document.querySelectorAll(".plus");
 
-buttons.forEach((button) => {
+buttons.forEach((button, index) => {
   button.addEventListener("click", function () {
     button.style.backgroundColor = "hsl(14, 86%, 42%)";
     button.style.color = "white";
+    button.style.borderColor = "hsl(14, 86%, 42%)";
     button.textContent = "1";
+
+    minusSymbols[index].style.display = "block";
+    plusSymbols[index].style.display = "block";
   });
 });
