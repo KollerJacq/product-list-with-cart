8a089861df49a5ec5dacc10011cf87755fe6cfa2
KollerJacq <147981831+KollerJacq@users.noreply.github.com>
2024-09-29 15:29:02 +0200

    fixed button css for clicking

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll(".btn");
+const cartSymbol = document.querySelectorAll(".btnimg");
 let minusSymbols = document.querySelectorAll(".minus");
 let plusSymbols = document.querySelectorAll(".plus");
 
@@ -8,6 +9,8 @@ buttons.forEach((button, index) => {
     button.style.color = "white";
     button.style.borderColor = "hsl(14, 86%, 42%)";
     button.querySelector(".add").textContent = "1";
+    button.querySelector(".add").style.paddingRight = "30px";
+    cartSymbol[index].style.display = "none";
     minusSymbols[index].style.display = "block";
     plusSymbols[index].style.display = "block";
 
