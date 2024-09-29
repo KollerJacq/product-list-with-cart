ada11b0525a9af3eb9529b68744dd6a10fc46858
KollerJacq <147981831+KollerJacq@users.noreply.github.com>
2024-09-29 16:25:57 +0200

    added counter so buttons

--- script.js
+++ script.js
@@ -8,12 +8,36 @@ buttons.forEach((button, index) => {
     button.style.backgroundColor = "hsl(14, 86%, 42%)";
     button.style.color = "white";
     button.style.borderColor = "hsl(14, 86%, 42%)";
-    button.querySelector(".add").textContent = "1";
     button.querySelector(".add").style.paddingRight = "30px";
     cartSymbol[index].style.display = "none";
     minusSymbols[index].style.display = "block";
     plusSymbols[index].style.display = "block";
+  });
+});
 
-    console.log(plusSymbols);
+buttons.forEach((button, index) => {
+  let count = 0;
+
+  const addText = button.querySelector(".add");
+
+  button.addEventListener("click", function () {
+    if (count === 0) {
+      count = 1;
+      addText.textContent = count;
+    }
+  });
+
+  plusSymbols[index].addEventListener("click", function (event) {
+    event.stopPropagation();
+    count++;
+    addText.textContent = count;
+  });
+
+  minusSymbols[index].addEventListener("click", function (event) {
+    event.stopPropagation();
+    if (count > 0) {
+      count--;
+      addText.textContent = count;
+    }
   });
 });
